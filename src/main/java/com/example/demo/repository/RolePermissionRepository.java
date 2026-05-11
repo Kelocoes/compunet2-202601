8969d11f778b94e7387b625e29a1b91a3dec028e
kelocoes <kevin.rodriguez109@gmail.com>
2026-05-11 17:38:41 -0500

    React

--- src/main/java/com/example/demo/repository/RolePermissionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.RolePermission;
-
-public interface RolePermissionRepository extends JpaRepository<RolePermission, Long> {
-}
